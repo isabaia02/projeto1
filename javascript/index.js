255aa1b667ee26cfeb71d648084949374c2756df
Andre Tavares <andre.tavares@digitalsys.com.br>
2024-06-21 15:57:29 -0300

    Now users can no longer add empty values into the field

--- javascript/index.js
+++ javascript/index.js
@@ -1,82 +1,87 @@
 document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    // Get input values
-   
-    var name = document.getElementById('fname').value;
-    var email = document.getElementById('email').value;
+  // Get input values
 
-    // Get current date and format it to dd/mm/yyyy
-    var date = new Date();
-    var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear();
+  var name = document.getElementById('fname').value;
+  var email = document.getElementById('email').value;
 
-    // Create user object
-    var user = {name: name, email: email, date: formattedDate};
+  if (name == '' || email == '') {
+    alert('Por favor, preencha todos os campos!!!')
+    return false;
+  }
 
-    // Get existing users from local storage or create new array if none exist
-    var users = JSON.parse(localStorage.getItem('users')) || [];
+  // Get current date and format it to dd/mm/yyyy
+  var date = new Date();
+  var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear();
 
-    // Add new user to users array
-    users.push(user);
+  // Create user object
+  var user = { name: name, email: email, date: formattedDate };
 
-    // Save new users array to local storage
-    localStorage.setItem('users', JSON.stringify(users));
+  // Get existing users from local storage or create new array if none exist
+  var users = JSON.parse(localStorage.getItem('users')) || [];
 
-    // Clear input fields
-    document.getElementById('fname').value = '';
-    document.getElementById('email').value = '';
+  // Add new user to users array
+  users.push(user);
 
-    // Display users in box_list div
-    displayUsers();
+  // Save new users array to local storage
+  localStorage.setItem('users', JSON.stringify(users));
+
+  // Clear input fields
+  document.getElementById('fname').value = '';
+  document.getElementById('email').value = '';
+
+  // Display users in box_list div
+  displayUsers();
 });
 
 document.getElementById('input_search').addEventListener('input', function() {
-    displayUsers(this.value);
+  displayUsers(this.value);
 });
 
 function displayUsers(searchValue = '') {
-    var users = JSON.parse(localStorage.getItem('users')) || [];
-    var boxList = document.getElementById('box_list_child');
-    boxList.innerHTML = '';
-    users.forEach(function(user, index) {
-        if (user.name.includes(searchValue) || user.email.includes(searchValue)) {
-            var userDiv = document.createElement('div');
-            userDiv.classList.add('userList');
-            var spanName= document.createElement('span');
-            spanName.textContent = 'Name: ' + user.name;
-            var spanEmail= document.createElement('span');
-            spanEmail.textContent = 'Email: ' + user.email;
-            var spanDate= document.createElement('span');
-            spanDate.textContent = 'Date: ' + user.date;
-            var deleteButton = document.createElement('button');
-            deleteButton.textContent = 'x';
-            deleteButton.classList.add('closeUserList');
-            deleteButton.addEventListener('click', function() {
-                deleteUser(index);
-            });
-            userDiv.appendChild(spanName);
-            userDiv.appendChild(spanDate);
-            userDiv.appendChild(spanEmail);
-            userDiv.appendChild(deleteButton);
-            boxList.appendChild(userDiv);
-        }
-    });
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+  var boxList = document.getElementById('box_list_child');
+  boxList.innerHTML = '';
+  users.forEach(function(user, index) {
+    if (user.name.includes(searchValue) || user.email.includes(searchValue)) {
+      var userDiv = document.createElement('div');
+      userDiv.classList.add('userList');
+      var spanName = document.createElement('span');
+      spanName.textContent = 'Name: ' + user.name;
+      var spanEmail = document.createElement('span');
+      spanEmail.textContent = 'Email: ' + user.email;
+      var spanDate = document.createElement('span');
+      spanDate.textContent = 'Date: ' + user.date;
+      var deleteButton = document.createElement('button');
+      deleteButton.textContent = 'x';
+      deleteButton.classList.add('closeUserList');
+      deleteButton.addEventListener('click', function() {
+        deleteUser(index);
+      });
+      userDiv.appendChild(spanName);
+      userDiv.appendChild(spanDate);
+      userDiv.appendChild(spanEmail);
+      userDiv.appendChild(deleteButton);
+      boxList.appendChild(userDiv);
+    }
+  });
 }
 
 function deleteUser(index) {
-    var users = JSON.parse(localStorage.getItem('users')) || [];
-    users.splice(index, 1);
-    localStorage.setItem('users', JSON.stringify(users));
-    displayUsers();
+  var users = JSON.parse(localStorage.getItem('users')) || [];
+  users.splice(index, 1);
+  localStorage.setItem('users', JSON.stringify(users));
+  displayUsers();
 }
 
 // Display users when page loads
 displayUsers();
 
 var closeAll = document.getElementById('bottom_CloseAll');
-closeAll.addEventListener('click', function(){
-    document.querySelectorAll('.userList').forEach((element) =>{
-        deleteUser(0);
-    })
-    
+closeAll.addEventListener('click', function() {
+  document.querySelectorAll('.userList').forEach((element) => {
+    deleteUser(0);
+  })
+
 });
